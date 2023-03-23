7b559a6b030d1da25349e58c1c33b3d98196cf13
antieatingactivist <antieatingactivist@gmail.com>
2023-03-23 12:55:19 -0700

    dragging partially works

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,26 +3,63 @@ import './App.css';
 
 function App() {
   const videoElement = useRef<HTMLVideoElement>(null);
-  const [width, setWidth] = useState(100);
-  const [height, setHeight] = useState(100);
+  const [width, setWidth] = useState(150);
+  const [height, setHeight] = useState(150);
 
   useEffect(() => {
     getVideo();
-    getDevices();
-    // setTimeout(() => {
-    //   navigator.mediaDevices.
-    // },2000)
+    // getDevices();
+
+
+    let mouseDown = false;
+    let startX = 0;
+    let startY = 0;
+    let scrollLeft = 0;
+    let scrollTop = 0;
+    // const windowEl = window as unknown as HTMLElement;
+    
+    let startDragging = function (e: any) {
+      mouseDown = true;
+      startX = e.pageX - window.pageXOffset;
+      startY = e.pageY - window.pageYOffset;
+      scrollLeft = window.scrollX;
+      scrollTop = window.scrollY;
+    };
+    let stopDragging = function (event:any) {
+      mouseDown = false;
+    };
+    
+    window.addEventListener('mousemove', (e) => {
+      e.preventDefault();
+      if(!mouseDown) { return; }
+      console.log(e);
+      const x = e.pageX - window.pageXOffset;
+      const y = e.pageY - window.pageYOffset;
+      const scrollX = x - startX;
+      const scrollY = y - startY;
+      window.scrollTo(scrollLeft - scrollX, scrollTop - scrollY);
+    });
+    
+    // Add the event listeners
+    window.addEventListener('mousedown', startDragging, false);
+    window.addEventListener('mouseup', stopDragging, false);
+    window.addEventListener('mouseleave', stopDragging, false);
+
+
+
+
+
+
   },[]);
+
   function getVideo() {
     navigator.mediaDevices
       .getUserMedia({ 
         video: {
           deviceId: {
-            // exact: "8772118CA0398C5472E334DF14A5C527F57AE791",
-            exact: "f6645729ac74f1e32f0c8d7bf2c193a57a08b2d70a48844e686b3a7f73d41aac"
+            exact: "8772118CA0398C5472E334DF14A5C527F57AE791",
+            // exact: "f6645729ac74f1e32f0c8d7bf2c193a57a08b2d70a48844e686b3a7f73d41aac"
           },
-          // width: { min: 1280 },
-          // height: { min: 720 },
         },
         
       })
@@ -30,7 +67,6 @@ function App() {
         let video = videoElement.current;
         if (video) {
           video.srcObject = stream;
-          // video.play();
         }
       })
       .catch(err => {
@@ -38,9 +74,10 @@ function App() {
       });
   };
   function zoomIn() {
-    setHeight(height+10);
-    setWidth(width+10);
+    // setHeight(height+10);
+    // setWidth(width+10);
   }
+
   async function getDevices() {
     const devices = await navigator.mediaDevices.enumerateDevices();
     console.log(devices);
