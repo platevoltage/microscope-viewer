3e3ea20147d717596047dd966b7af88e2b1a8faf
antieatingactivist <antieatingactivist@gmail.com>
2023-03-22 23:49:04 -0700

    camera now works

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,12 +1,31 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const videoElement = useRef<HTMLVideoElement>(null);
 
-
+  useEffect(() => {
+    getVideo();
+  },[]);
+  function getVideo() {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then(stream => {
+        let video = videoElement.current;
+        if (video) {
+          video.srcObject = stream;
+          video.play();
+        }
+      })
+      .catch(err => {
+        console.error("error:", err);
+      });
+  };
   return (
-    <div className="App">
-
+    <div className="App" id="container">
+      <video autoPlay={true} id="videoElement" ref={videoElement}>
+      
+      </video>
     </div>
   );
 }
