b53173e88e640149f694fad14ae0639d3611c6e5
antieatingactivist <antieatingactivist@gmail.com>
2023-03-23 17:01:26 -0700

    camera now selectable

--- react/src/components/HUD.tsx
+++ react/src/components/HUD.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import {useState, useEffect, useRef} from 'react';
 import './HUD.css'
 
 interface Props {
     zoomIn: () => void;
     zoomOut: () => void;
+    device?: MediaDeviceInfo;
+    setDevice(device: MediaDeviceInfo): void;
+    deviceList?: MediaDeviceInfo[];
+    setDeviceList(deviceList: MediaDeviceInfo[]): void;
 }
-export default function HUD({zoomIn, zoomOut}: Props) {
-  return (
-    <div className="HUD">
-      <button onClick={zoomIn}>+</button>
-      <button onClick={zoomOut}>-</button>
-    </div>
-  )
+
+async function getDevices() {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return devices;
+}
+
+
+export default function HUD({zoomIn, zoomOut, device, setDevice, deviceList, setDeviceList}: Props) {
+    // const [deviceList, setDeviceList] = useState<MediaDeviceInfo[]>();
+    // const [device, setDevice] = useState<string>();
+
+    function handleChange(e: any) {
+        const index: number = e.target.value;
+        console.log(index);
+        if (deviceList) setDevice(deviceList[index]);
+    }
+
+
+    return (
+        <div className="HUD">
+            <button onClick={zoomIn}>+</button>
+            <button onClick={zoomOut}>-</button>
+            <select name="device-select" id="device-select" onChange={handleChange}>
+
+            {deviceList?.map((device, i) =>
+                <option key={i} value={i}>
+                    {device.label}
+                </option>    
+            )}
+            </select>
+            {device?.deviceId}
+        </div>
+    )
 }
