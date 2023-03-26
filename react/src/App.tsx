f8bbbdd3ec2eb49b782fa44704941588835cce3c
antieatingactivist <antieatingactivist@gmail.com>
2023-03-26 11:56:13 -0700

    fixed drag bar

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -116,11 +116,12 @@ function App() {
   
   return (
     <div className="App" onMouseEnter={() => setShowHUD(true)} onMouseLeave={() => setShowHUD(false)}>
+      
+      <div className="drag"></div>
 
       <div onClick={() => setSnapshotToShow(-1)}>
         <Video zoom={zoom} angle={angle} device={device} addImage={addImage} takeSnapshot={takeSnapshot} snapshotToShow={snapshots[snapshotToShow]} />
       </div>
-      <div className="drag"></div>
       <div style={{position: "absolute", transitionProperty: "opacity, left", transitionDuration: ".5s, .1s", opacity: showHUD ? 1 : 0, left: `${showSidebar ? 11 : 1}em`, zIndex: "1"}}>
         <HUD zoomIn={zoomIn} zoomOut={zoomOut} rotateCCW={rotateCCW} rotateCW={rotateCW} device={device} setDevice={setDevice} deviceList={deviceList} setDeviceList={setDeviceList} takeSnapshot={snapshot} showSnapshot={snapshotToShow > -1}/>
         {!showSidebar && <SidebarIcon toggleSidebar={toggleSidebar} />}
