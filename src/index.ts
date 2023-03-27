bacbdc77d056efc3ceddfa2929507062ea524aed
antieatingactivist <antieatingactivist@gmail.com>
2023-03-26 20:55:09 -0700

    moved menu config to new file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow, Menu, systemPreferences } from 'electron';
 import * as path from 'path';
+import { menuConfig } from './menu';
 
 const isMac = process.platform === 'darwin';
 
@@ -40,6 +41,13 @@ const createWindow = () => {
       
 };
 
+app.setAboutPanelOptions({
+  applicationName: "Microscope Viewer", 
+  applicationVersion: "Version",
+  version: "1.0.0",
+  // credits: "Credits",
+  copyright: "(c) Garrett Corbin 2023"
+});
 
 app.whenReady().then(async () => {
 
@@ -53,113 +61,10 @@ app.whenReady().then(async () => {
   win.show();
   
   // note: your contextMenu, Tooltip and Title code will go here!
-  const template: any = [
-    // { role: 'appMenu' }
-    ...(isMac ? [{
-      label: app.name,
-      submenu: [
-        { role: 'about' },
-        { type: 'separator' },
-        { role: 'services' },
-        { type: 'separator' },
-        { role: 'hide' },
-        { role: 'hideOthers' },
-        { role: 'unhide' },
-        { type: 'separator' },
-        { role: 'quit' }
-      ]
-    }] : []),
-    // { role: 'fileMenu' }
-    {
-      label: 'File',
-      submenu: [
-        isMac ? { role: 'close' } : { role: 'quit' }
-      ]
-    },
-    // { role: 'editMenu' }
-    {
-      label: 'Edit',
-      submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' },
-        ...(isMac ? [
-          { role: 'pasteAndMatchStyle' },
-          { role: 'delete' },
-          { role: 'selectAll' },
-          { type: 'separator' },
-          {
-            label: 'Speech',
-            submenu: [
-              { role: 'startSpeaking' },
-              { role: 'stopSpeaking' }
-            ]
-          }
-        ] : [
-          { role: 'delete' },
-          { type: 'separator' },
-          { role: 'selectAll' }
-        ])
-      ]
-    },
-    // { role: 'viewMenu' }
-    {
-      label: 'View',
-      submenu: [
-        // { role: 'reload' },
-        // { role: 'forceReload' },
-        // { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' }
-      ]
-    },
-    // { role: 'windowMenu' }
-    {
-      label: 'Window',
-      submenu: [
-        { role: 'minimize' },
-        { role: 'zoom' },
-        ...(isMac ? [
-          { type: 'separator' },
-          { role: 'front' },
-          { type: 'separator' },
-          { role: 'window' }
-        ] : [
-          { role: 'close' }
-        ])
-      ]
-    },
-    {
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click: async () => {
-            const { shell } = require('electron')
-            await shell.openExternal('https://electronjs.org')
-          }
-        }
-      ]
-    }
-  ]
   
-  const menu = Menu.buildFromTemplate(template);
-  // const menu = Menu.buildFromTemplate([
-  //   {
-  //     label: app.name, 
-  //     submenu: [
-  //      { label: "Quit", role: 'quit' }
-  //     ]
-  //   }
-  // ]);
+  const menu = Menu.buildFromTemplate(menuConfig);
   Menu.setApplicationMenu(menu);
+
   systemPreferences.askForMediaAccess("camera");
 })
 
