cb0c5654cef0afe5ab3af6b0af01b4d6780b9824
antieatingactivist <antieatingactivist@gmail.com>
2023-03-29 17:58:06 -0700

    mocked up nag screen

--- react/src/Nagware.tsx
+++ react/src/Nagware.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import './Nagware.css';
 export default function Nagware() {
   return (
-    <div>
-      Pay Me!!
+    <div className="nagware">
+      <div className="message">I would really appreciate if you have me lots of money</div>
+
+      <div className="buttons"><button>Quit</button></div>
     </div>
   )
 }
